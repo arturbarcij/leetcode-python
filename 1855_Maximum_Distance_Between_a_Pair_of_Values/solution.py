feb742eb1b4c06fb71c1fe69c719206a964f4909
arturbarcij <arturbarcij1@gmail.com>
2026-04-20 03:39:51 +0200

    comments

--- 1855_Maximum_Distance_Between_a_Pair_of_Values/solution.py
+++ 1855_Maximum_Distance_Between_a_Pair_of_Values/solution.py
@@ -1,3 +1,7 @@
 class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
-        pass
+        # pairs (i, j)
+        # 0 <= j < nums2.length
+        # if i <= j and nums1[i] <= nums2[j]
+        # distance j - i
+        # return max distance of (i, j) else return 0
