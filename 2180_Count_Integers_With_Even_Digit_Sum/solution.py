c8e08adce9489cf4f6d95a8d5baf7bb3fa9a9f7d
arturbarcij <arturbarcij1@gmail.com>
2026-05-22 23:32:58 +0200

    count even test cases

---
+++ 2180_Count_Integers_With_Even_Digit_Sum/solution.py
@@ -0,0 +1,18 @@
+'''
+Example 1:
+
+Input: num = 4
+Output: 2
+Explanation:
+The only integers less than or equal to 4 whose digit sums are even are 2 and 4.    
+Example 2:
+
+Input: num = 30
+Output: 14
+Explanation:
+The 14 integers less than or equal to 30 whose digit sums are even are
+2, 4, 6, 8, 11, 13, 15, 17, 19, 20, 22, 24, 26, and 28.
+'''
+
+class Solution:
+    def countEven(self, num: int) -> int:
